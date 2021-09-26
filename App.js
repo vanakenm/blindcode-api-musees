3d1a999d34dcd411a948466b98aecffa13b3f73f
Martin Van Aken <martin@joyouscoding.com>
2021-09-26 09:46:46 +0200

    Use more complete API

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+const URL = "https://opendata.bruxelles.be/api/records/1.0/search/?dataset=museums-in-brussels&q="
+
 export default function App() {
+  const [musees, setMusees] = useState([]);
+  async function fetchData() {
+    let response = await fetch(URL);
+    let data = await response.json();
+    setMusees(data.records)
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, [])
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      { musees.map(musee => (
+        <Text key={musee.recordid}>{musee.fields.nom_du_musee}</Text>
+      ))}
       <StatusBar style="auto" />
     </View>
   );
