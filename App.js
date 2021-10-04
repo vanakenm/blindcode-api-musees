22457d5d16f52fd444f9dde44f0992d42778283a
Martin Van Aken <martin@joyouscoding.com>
2021-10-04 13:24:40 +0200

    Add button import

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 const URL = "https://opendata.bruxelles.be/api/records/1.0/search/?dataset=museums-in-brussels&q="
 
